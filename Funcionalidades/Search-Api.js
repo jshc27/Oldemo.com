0f562002cf96302144c39ca773ff105989efd369
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-09-16 16:37:51 -0500

    added fake API and style cards

--- Funcionalidades/Search-Api.js
+++ Funcionalidades/Search-Api.js
@@ -21,3 +21,25 @@ function searchClass() {
     }
 
 }
+
+
+// FAKE API
+
+const url = "https://www.etnassoft.com/api/v1/get/?category=libros_programacion&criteria=most_viewed";
+
+fetch(url)
+.then( response => response.json())
+.then(data =>{
+    console.log(data);
+
+    let image1 = document.getElementById('image1');
+    image1.innerHTML = `<img width="100px" height="100px" src='${data[0].cover}'/>`;
+
+    let title1 = document.getElementById('title1');
+    title1.innerHTML = `${data[0].title}`;
+
+    let text1 = document.getElementById('text1');
+    text1.innerHTML = `${data[0].content_short}`;
+
+})
+
