fd7ef728eeef59f872986d7fac2c10a36c28da03
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-09-17 15:15:12 -0500

    added changes API cards 1234

--- Funcionalidades/Search-Api.js
+++ Funcionalidades/Search-Api.js
@@ -32,6 +32,9 @@ fetch(url)
 .then(data =>{
     console.log(data);
 
+
+    // card1 
+
     let image1 = document.getElementById('image1');
     image1.innerHTML = `<img width="100px" height="100px" src='${data[0].cover}'/>`;
 
@@ -41,5 +44,40 @@ fetch(url)
     let text1 = document.getElementById('text1');
     text1.innerHTML = `${data[0].content_short}`;
 
-})
+
+    //card2
+
+    let image2 = document.getElementById('image2');
+    image2.innerHTML =  `<img width="100px" height="100px" src='${data[1].cover}'/>`;
+
+    let title2 = document.getElementById('title2');
+    title2.innerHTML =  `${data[1].title}`;
+
+    let text2 = document.getElementById('text2');
+    title2.innerHTML =  `${data[1].content_short}`;
+
+    //card3
+
+    let image3 = document.getElementById('image3');
+    image3.innerHTML =  `<img width="100px" height="100px" src='${data[2].cover}'/>`;
+
+    let title3 = document.getElementById('title3');
+    title2.innerHTML =  `${data[2].title}`;
+
+    let text3 = document.getElementById('text3');
+    title3.innerHTML =  `${data[2].content_short}`;
+
+     //card4
+
+    let image4 = document.getElementById('image4');
+    image4.innerHTML =  `<img width="100px" height="100px" src='${data[3].cover}'/>`;
+
+    let title4 = document.getElementById('title4');
+    title4.innerHTML =  `${data[3].title}`;
+
+    let text4 = document.getElementById('text4');
+    title4.innerHTML =  `${data[3].content_short}`;
+
+
+});
 
